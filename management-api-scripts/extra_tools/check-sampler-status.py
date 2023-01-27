71c60a47525e78a01eb4e62f7e38ec0dc10414ac
phev8 <phev8@users.noreply.github.com>
2023-01-27 14:32:28 +0100

    remove example code and use cli arguments

--- management-api-scripts/extra_tools/check-sampler-status.py
+++ management-api-scripts/extra_tools/check-sampler-status.py
@@ -1,22 +1,32 @@
 import requests
 from datetime import timedelta
-
-api_key = "secret-api-key1"
-url = 'http://localhost:5015/sampler/germany/status'
-
-r = requests.get(url, headers={
-                    "Api-Key": api_key
-                },
-            )
-
-if r.status_code != 200:
-    print(r.content)
-    exit()
-resp = r.json()
+import argparse
 
 
-print('\n\nTime since beginning of the interval: {} ({}s)'.format(timedelta(seconds=resp['currentTimeInInterval']), resp['currentTimeInInterval'] ))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--status_url", help="url where status can be checked", required=True)
+    parser.add_argument("--api_key", help="use this api key", required=True)
 
-print('Slots (target currently - used = available): {} - {} = {}'.format(resp['openSlotsTarget'], resp['usedSlots'], resp['availableSlots']))
-print('Max slots at the end of interval: {}'.format(resp['maxSlots']))
+    args = parser.parse_args()
+
+
+    url = args.status_url
+    api_key = args.api_key
+
+    r = requests.get(url, headers={
+                        "Api-Key": api_key
+                    },
+                )
+
+    if r.status_code != 200:
+        print(r.content)
+        exit()
+    resp = r.json()
+
+
+    print('\n\nTime since beginning of the interval: {} ({}s)'.format(timedelta(seconds=resp['currentTimeInInterval']), resp['currentTimeInInterval'] ))
+
+    print('Slots (target currently - used = available): {} - {} = {}'.format(resp['openSlotsTarget'], resp['usedSlots'], resp['availableSlots']))
+    print('Max slots at the end of interval: {}'.format(resp['maxSlots']))
 
